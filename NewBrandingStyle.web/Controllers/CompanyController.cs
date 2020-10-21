f208bd5d7724d21b21efe3b428f88b9e1338dba8
liwiakawa <liwia.kawa@gmail.com>
2020-10-21 18:56:23 +0200

    init

---
+++ NewBrandingStyle.web/Controllers/CompanyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.web.Models;
+
+namespace NewBrandingStyle.web.Controllers
+{
+
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
+
+        }
+    }
+}
